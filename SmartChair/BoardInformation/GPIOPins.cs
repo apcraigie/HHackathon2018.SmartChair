1e5e4fc34c9b1a7897186fc92dc2cf29c4cb10f4
Hunter McCullough <huntermccullough56@gmail.com>
2018-10-14 09:55:05 -0700

    Added new UWP static library Removed unnecessary folder Renamed folder more appropriately

---
+++ SmartChair/BoardInformation/GPIOPins.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BoardInformation
+{
+    public enum GPIOPins
+    {
+        SitButton, Beeper, LiquidSensor, DistanceSensor, TemperatureSensor, SoundSensor
+    };
+    public static class PinInformation
+    {
+        public static Dictionary<GPIOPins, List<int>> PinMappings = new Dictionary<GPIOPins, List<int>>
+        {
+            { GPIOPins.SitButton, new List<int>{22} },
+            { GPIOPins.Beeper, new List<int>{10} },
+            { GPIOPins.LiquidSensor, new List<int>{9} },
+            { GPIOPins.DistanceSensor, new List<int>{5, 6} },
+            { GPIOPins.TemperatureSensor, new List<int>{19} },
+            { GPIOPins.SoundSensor, new List<int>{21} }
+        };
+    }
+}
---
+++ SmartChair/MusicalChair/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using BoardInformation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MusicalChair
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<int> _distancePins = PinInformation.PinMappings[GPIOPins.DistanceSensor];
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
